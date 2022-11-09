13a235439d7460d3a085965af34fd9864bbd2524
yannick <malikluc@tutanota.com>
2022-11-10 00:26:32 +0100

    removed Constructor

--- src/GoGame/GameStateException.java
+++ src/GoGame/GameStateException.java
@@ -3,5 +3,4 @@ package GoGame;
 public class GameStateException extends Exception {
     public GameStateException() { super(); }
     public GameStateException(String message) { super(message); }
-    public GameStateException(String message, Throwable t) { super(message, t); }
 }
